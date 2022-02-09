1204a4fb715c6886b14256497c8cb1e8a664fd64
oliwiamich <omichalak@magicseaweed.com>
2022-02-09 16:12:34 +0000

    feat: models and stubs

---
+++ TastySnacks/TastySnacks/Model/TastySnack.swift
@@ -0,0 +1,34 @@
+//
+//  TastySnack.swift
+//  TastySnacks
+//
+//  Created by Oliwia Michalak on 09/02/2022.
+//
+
+import Foundation
+
+struct TastySnack: Decodable {
+    let id: Int
+    let name: String
+    let description: String
+    let price: Double
+    let imageURL: String
+    let calories: Int
+    let protein: Int
+    let carbs: Int
+}
+
+struct MockData {
+    static let sampleSnack = TastySnack(
+        id: 123,
+        name: "test snack",
+        description: "super tasty, this a description for the snack!",
+        price: 9.99,
+        imageURL: "",
+        calories: 99,
+        protein: 99,
+        carbs: 99
+    )
+
+    static let snapleSnacks = [sampleSnack, sampleSnack, sampleSnack, sampleSnack]
+}
---
+++ TastySnacks/TastySnacks/Model/TastySnackResponse.swift
@@ -0,0 +1,12 @@
+//
+//  TastySnackResponse.swift
+//  TastySnacks
+//
+//  Created by Oliwia Michalak on 09/02/2022.
+//
+
+import Foundation
+
+struct TastySnackResponse {
+    let request: [TastySnack]
+}
