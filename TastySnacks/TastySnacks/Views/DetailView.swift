f07e60aae74645071096f3e70596fd529d84bc7a
oliwiamich <omichalak@magicseaweed.com>
2022-02-11 14:35:55 +0000

    feat: initial detail view UI

---
+++ TastySnacks/TastySnacks/Views/DetailView.swift
@@ -0,0 +1,101 @@
+//
+//  DetailView.swift
+//  TastySnacks
+//
+//  Created by Oliwia Michalak on 11/02/2022.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let tastySnack: TastySnack
+
+    var body: some View {
+        VStack {
+            Image("test-image")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 300, height: 225)
+            VStack {
+                Text(tastySnack.name)
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                
+                Text(tastySnack.description)
+                    .multilineTextAlignment(.center)
+                    .font(.body)
+                    .padding()
+                HStack(spacing: 40) {
+                    VStack(spacing: 5) {
+                        Text("Calories")
+                            .bold()
+                            .font(.caption)
+                        Text("\(tastySnack.calories)")
+                            .foregroundColor(.secondary)
+                            .fontWeight(.semibold)
+                            .italic()
+                    }
+                    
+                    VStack(spacing: 5) {
+                        Text("Carbs")
+                            .bold()
+                            .font(.caption)
+                        Text("\(tastySnack.carbs)")
+                            .foregroundColor(.secondary)
+                            .fontWeight(.semibold)
+                            .italic()
+                    }
+                    
+                    VStack(spacing: 5) {
+                        Text("Protein")
+                            .bold()
+                            .font(.caption)
+                        Text("\(tastySnack.protein)")
+                            .foregroundColor(.secondary)
+                            .fontWeight(.semibold)
+                            .italic()
+                    }
+                }
+            }
+
+            Spacer()
+
+            Button {
+                print("test")
+            } label: {
+                Text("$\(tastySnack.price, specifier: "%.2f") - Add to Order")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .frame(width: 260, height: 50)
+                    .foregroundColor(.white)
+                    .background(Color.brandPrimary)
+                    .cornerRadius(10)
+            }
+            .padding(.bottom, 30)
+        }
+        .frame(width: 300, height: 525)
+        .background(Color(.systemBackground))
+        .cornerRadius(12)
+        .shadow(radius: 40)
+        .overlay(Button {
+            print("dismiss")
+        } label: {
+            ZStack {
+                Circle()
+                    .frame(width: 30, height: 30)
+                    .foregroundColor(.white)
+                    .opacity(0.6)
+                Image(systemName: "xmark")
+                    .imageScale(.small)
+                    .frame(width: 44, height: 44)
+                foregroundColor(.black)
+            }
+        }, alignment: .topTrailing)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(tastySnack: MockData.sampleSnack)
+    }
+}
