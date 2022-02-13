ac0e089c765c3f19e0f51b425488b353eaba124d
oliwiamich <omichalak@magicseaweed.com>
2022-02-13 18:38:38 +0000

    feat: account view UI

--- TastySnacks/TastySnacks/Views/AccountView.swift
+++ TastySnacks/TastySnacks/Views/AccountView.swift
@@ -8,10 +8,40 @@
 import SwiftUI
 
 struct AccountView: View {
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var birthdate = Date()
+    @State private var refill = false
+    @State private var dessert = false
+
     var body: some View {
         NavigationView {
-            Text("Accont")
-                .navigationTitle("Account")
+            Form {
+                Section(header: Text("Personal information")) {
+                    TextField("First name", text: $firstName)
+                    TextField("Last name", text: $lastName)
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .autocapitalization(.none)
+                        .disableAutocorrection(true)
+
+                    DatePicker("Birthday", selection: $birthdate, displayedComponents: .date)
+
+                    Button {
+                        print("save")
+                    } label: {
+                        Text("Save changes")
+                    }
+                }
+
+                Section(header: Text("Request")) {
+                    Toggle("Refill", isOn: $refill)
+                    Toggle("Dessert menu", isOn: $dessert)
+                }
+                .toggleStyle(SwitchToggleStyle(tint: .brandPrimary))
+            }
+            .navigationTitle("Account")
         }
     }
 }
